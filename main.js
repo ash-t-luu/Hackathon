4ab14f36f45761a381d30377c90320c62d69b143
ash-t-luu <ashleyluu87@gmail.com>
2023-07-21 21:03:49 -0700

    sending over updated functionality

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 //creating a fetch to see the weather data
 
 // https://api.open-meteo.com/v1/forecast?latitude=36.175&longitude=-115.1372&hourly=temperature_2m&temperature_unit=fahrenheit&precipitation_unit=inch&timeformat=unixtime&forecast_days=1
@@ -7,93 +5,103 @@
 //write logic to access geolocation of the computer using navigator.geolocation
 // login for open-meteo 
 
-fetch('https://api.open-meteo.com/v1/forecast?latitude=36.175&longitude=-115.1372&hourly=temperature_2m&daily=weathercode&temperature_unit=fahrenheit&precipitation_unit=inch&timezone=America%2FLos_Angeles&forecast_days=1')
+//creating functionality to get the latitude and longitude of the user's location
+//first cb is success, second is error
+
+// let latitude;
+// let longitude;
+
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//         console.log(position);
+//         latitude = position.coords.latitude;
+//         longitude = position.coords.longitude;
+//         console.log(latitude, longitude);
+//     }, function () {
+//         alert('Could not get your position');
+//     })
+// }
+
+// 'https://api.open-meteo.com/v1/forecast?latitude=36.175&longitude=-115.1372&hourly=temperature_2m&daily=weathercode&temperature_unit=fahrenheit&precipitation_unit=inch&timezone=America%2FLos_Angeles&forecast_days=1'
+
+fetch('https://api.open-meteo.com/v1/forecast?latitude=33.793036&longitude=-117.9673424&hourly=temperature_2m&daily=weathercode&temperature_unit=fahrenheit&precipitation_unit=inch&timezone=America%2FLos_Angeles&forecast_days=1')
     .then((response) => response.json())
     .then((json) => {
         const weatherObj = json;
         console.log(weatherObj);
-        console.log(weatherObj.hourly.time[0]);
-        console.log(weatherObj.hourly.temperature_2m[0]);
-        console.log(weatherObj.daily.weathercode);
+        // console.log(weatherObj.hourly.time);
+        // console.log(weatherObj.hourly.temperature_2m);
 
-        //create element to store temperature h2?
-        //create innerhtml content to store temp
-        //create element to store time h3
-        //create innerhtml content to store time
+        //hourly: {time: array(24), temp: array (24)}
+        //for the pop up - make it display the temperature at the time of the hour 
+
+        const lastFive = [];
+        //iterating over the time array to change the string to just the hours
+        for (let str of weatherObj.hourly.time) {
+            lastFive.push(str.slice(str.length - 5));
+        }
+        console.log('new time arr', lastFive);
+
+        //get current hour and compare to the time to do the functionality below 
+        const date = new Date().toTimeString();
+        let currentTime = date.slice(0, 5);
+        let numberCurrTime = parseInt(currentTime);
+        // console.log('numberCurrTime', numberCurrTime);
+
+        currentTime = Math.floor(numberCurrTime);
+        // console.log(currentTime);
+        // console.log(currentTime);
+
+        // const currentMinute = date.slice(3, 5);
+        // console.log(currentMinute);
+
+        // let currentHour = date.slice(0, 2);
+        // console.log(currentHour);
+
+        // if (Number(currentMinute) >= 30) {
+        //     // console.log(currentMinute);
+        //     console.log(true)
+        //     console.log(Number(currentHour) += 1);
+        //     // console.log(Number(currentHour) + 1);
+        // }
 
         //how do we access the temperature array and time array to line up? by index? iterating? 
-        //use template literal to access the value from the object? 
+        //store in an object: key: time, value: temp
+        const timeAndTemp = {};
+
+        for (let i = 0; i <= 23; i++) {
+            timeAndTemp[i] = weatherObj.hourly.temperature_2m[i];
+        }
+        console.log(timeAndTemp);
+
+        const temperatureContent = document.querySelector('h2');
+        temperatureContent.innerHTML = `Outside temperature is ${timeAndTemp[currentTime]} °F`;
+        document.querySelector('#container').appendChild(temperatureContent);
+
+        console.log(weatherObj.daily.weathercode);
+
+        //create obj - key would be the code, value the image src
+        const weatherCodeObj = {
+            0: "images/vector-sun-icon.jpg",
+        }
+
+
+        //functionality
+        for (let key in weatherCodeObj) {
+            const image = document.createElement('img');
+            const imageDiv = document.createElement('div');
+            if (weatherObj.daily.weathercode === key) {
+                image.setAttribute('src', `${weatherCodeObj[key]}`);
+                image.setAttribute('id', 'sun');
+                document.querySelector('body').appendChild(imageDiv);
+                imageDiv.appendChild(image);
+            }
+        }
+
+
+
 
     })
     .catch((error) => {
         console.log(`Oh no! ${error}`);
     })
-
-
-    // {
-    //     "latitude": 36.16438,
-    //     "longitude": -115.143936,
-    //     "generationtime_ms": 0.17893314361572266,
-    //     "utc_offset_seconds": 0,
-    //     "timezone": "GMT",
-    //     "timezone_abbreviation": "GMT",
-    //     "elevation": 609,
-    //     "hourly_units": {
-    //         "time": "unixtime",
-    //         "temperature_2m": "°F"
-    //     },
-    //     "hourly": {
-    //         "time": [
-    //             1689897600,
-    //             1689901200,
-    //             1689904800,
-    //             1689908400,
-    //             1689912000,
-    //             1689915600,
-    //             1689919200,
-    //             1689922800,
-    //             1689926400,
-    //             1689930000,
-    //             1689933600,
-    //             1689937200,
-    //             1689940800,
-    //             1689944400,
-    //             1689948000,
-    //             1689951600,
-    //             1689955200,
-    //             1689958800,
-    //             1689962400,
-    //             1689966000,
-    //             1689969600,
-    //             1689973200,
-    //             1689976800,
-    //             1689980400
-    //         ],
-    //         "temperature_2m": [
-    //             113.5,
-    //             111.1,
-    //             111.3,
-    //             107.7,
-    //             103.7,
-    //             99.8,
-    //             99.3,
-    //             96.2,
-    //             95.4,
-    //             92.2,
-    //             91.8,
-    //             93.3,
-    //             91.1,
-    //             88.6,
-    //             92.5,
-    //             97.4,
-    //             101.9,
-    //             104.8,
-    //             106.7,
-    //             109.8,
-    //             111.5,
-    //             112.4,
-    //             113.5,
-    //             114.2
-    //         ]
-    //     }
-    // }
